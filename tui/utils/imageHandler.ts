297cf84f324f40ad3458ef68282957a1c484eb42
chanloveit <ch20041001@gmail.com>
2026-02-17 03:47:00 +0000

    fix: resize image

--- tui/utils/imageHandler.ts
+++ tui/utils/imageHandler.ts
@@ -5,8 +5,8 @@ export async function displayImage(url: string): Promise<string | null> {
 	try{
 		const body = await got(url).buffer();
 		const image = await terminalImage.buffer(body, {
-			width: 50,
-			height: 50,
+			width: 20,
+			height: 20,
 			preserveAspectRatio: true
 		});
 
