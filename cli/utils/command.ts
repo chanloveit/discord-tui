942980435d0bb5274c5b7acbf120f65aa4dcd959
이찬희 <ch20041001@gmail.com>
2026-03-02 11:06:23 +0000

    refactor: remove cli dir

--- cli/utils/command.ts
+++
@@ -1,8 +0,0 @@
-export function commands(): void{
-	console.log('Commands: ');
-	console.log('/servers - List all servers and channels');
-	console.log('/join <server>:<channel> - Join a channel to chat');
-	console.log('/leave - Leave current channel');
-	console.log('/help - Show commands');
-	console.log('/quit - Exit\n');
-};
--- cli/utils/formatter.ts
+++
@@ -1,7 +0,0 @@
-export function formatTime(timestamp: number): string{
-	return new Date(timestamp).toLocaleTimeString(undefined, {
-		hour: '2-digit',
-		minute: '2-digit',
-		hour12: false
-	});
-}
